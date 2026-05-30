b4cbe4d9d460121d1ceb9a9b4a809288ebe919cc
hanumesatru <hajumesatru@gmail.com>
2026-05-30 09:36:13 +0700

    Create genres.js

---
+++ src/data/genres.js
@@ -0,0 +1,110 @@
+export const genres = [
+  {
+    id: 10759,
+    name: "Action & Adventure",
+    name_th: "แอคชั่น & ผจญภัย",
+    slug: "action-adventure"
+  },
+  {
+    id: 16,
+    name: "Animation",
+    name_th: "แอนิเมชัน",
+    slug: "animation"
+  },
+  {
+    id: 35,
+    name: "Comedy",
+    name_th: "คอมเมดี้",
+    slug: "comedy"
+  },
+  {
+    id: 80,
+    name: "Crime",
+    name_th: "อาชญากรรม",
+    slug: "crime"
+  },
+  {
+    id: 99,
+    name: "Documentary",
+    name_th: "สารคดี",
+    slug: "documentary"
+  },
+  {
+    id: 18,
+    name: "Drama",
+    name_th: "ดราม่า",
+    slug: "drama"
+  },
+  {
+    id: 10751,
+    name: "Family",
+    name_th: "ครอบครัว",
+    slug: "family"
+  },
+  {
+    id: 10762,
+    name: "Kids",
+    name_th: "เด็ก",
+    slug: "kids"
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+    name_th: "ลึกลับ",
+    slug: "mystery"
+  },
+  {
+    id: 10763,
+    name: "News",
+    name_th: "ข่าว",
+    slug: "news"
+  },
+  {
+    id: 10764,
+    name: "Reality",
+    name_th: "เรียลลิตี้",
+    slug: "reality"
+  },
+  {
+    id: 10765,
+    name: "Sci-Fi & Fantasy",
+    name_th: "ไซไฟ & แฟนตาซี",
+    slug: "sci-fi-fantasy"
+  },
+  {
+    id: 10766,
+    name: "Soap",
+    name_th: "ละครน้ำเน่า",
+    slug: "soap"
+  },
+  {
+    id: 10767,
+    name: "Talk",
+    name_th: "ทอล์คโชว์",
+    slug: "talk"
+  },
+  {
+    id: 10768,
+    name: "War & Politics",
+    name_th: "สงคราม & การเมือง",
+    slug: "war-politics"
+  },
+  {
+    id: 37,
+    name: "Western",
+    name_th: "คาวบอยตะวันตก",
+    slug: "western"
+  },
+  {
+    id: 100001,
+    name: "Boy Love (BL)",
+    name_th: "บอยเลิฟ (BL)",
+    slug: "bl"
+  },
+  {
+    id: 100002,
+    name: "Girl Love (GL)",
+    name_th: "เกิร์ลเลิฟ (GL)",
+    slug: "gl"
+  }
+];
