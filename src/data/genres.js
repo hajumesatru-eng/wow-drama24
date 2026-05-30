75a985fd3a006acb4bd1739d8e2315470743d072
hajumesatru-eng <hajumesatru@gmail.com>
2026-05-30 14:35:10 +0700

    add genre

---
+++ src/data/genres.js
@@ -0,0 +1,114 @@
+export const genres = [
+  {
+    name: "Action & Adventure",
+    name_th: "แอคชั่น & ผจญภัย",
+    slug: "action-adventure"
+  },
+  {
+    name: "Animation",
+    name_th: "แอนิเมชัน",
+    slug: "animation"
+  },
+  {
+    name: "Comedy",
+    name_th: "คอมเมดี้",
+    slug: "comedy"
+  },
+  {
+    name: "Crime",
+    name_th: "อาชญากรรม",
+    slug: "crime"
+  },
+  {
+    name: "Documentary",
+    name_th: "สารคดี",
+    slug: "documentary"
+  },
+  {
+    name: "Drama",
+    name_th: "ดราม่า",
+    slug: "drama"
+  },
+  {
+    name: "Family",
+    name_th: "ครอบครัว",
+    slug: "family"
+  },
+  {
+    name: "Kids",
+    name_th: "เด็ก",
+    slug: "kids"
+  },
+  {
+    name: "Mystery",
+    name_th: "ลึกลับ",
+    slug: "mystery"
+  },
+  {
+    name: "Reality",
+    name_th: "เรียลลิตี้",
+    slug: "reality"
+  },
+  {
+    name: "Sci-Fi & Fantasy",
+    name_th: "ไซไฟ & แฟนตาซี",
+    slug: "sci-fi-fantasy"
+  },
+  {
+    name: "Soap",
+    name_th: "ละครน้ำเน่า",
+    slug: "soap"
+  },
+  {
+    name: "Talk",
+    name_th: "ทอล์คโชว์",
+    slug: "talk"
+  },
+  {
+    name: "War & Politics",
+    name_th: "สงคราม & การเมือง",
+    slug: "war-politics"
+  },
+  {
+    name: "Western",
+    name_th: "คาวบอยตะวันตก",
+    slug: "western"
+  },
+
+  // 🌸 CUSTOM GENRES (DRAMA ASIA STYLE)
+  {
+    name: "Romance",
+    name_th: "โรแมนซ์",
+    slug: "romance"
+  },
+  {
+    name: "Historical",
+    name_th: "ย้อนยุค",
+    slug: "historical"
+  },
+  {
+    name: "Fantasy Romance",
+    name_th: "แฟนตาซีโรแมนซ์",
+    slug: "fantasy-romance"
+  },
+  {
+    name: "School",
+    name_th: "วัยเรียน",
+    slug: "school"
+  },
+  {
+    name: "Thriller",
+    name_th: "ระทึกขวัญ",
+    slug: "thriller"
+  },
+  {
+    name: "Boy Love (BL)",
+    name_th: "บอยเลิฟ (BL)",
+    slug: "bl"
+  },
+  {
+    name: "Girl Love (GL)",
+    name_th: "เกิร์ลเลิฟ (GL)",
+    slug: "gl"
+  }
+];
