6f3f9afa84736c5a831c5057af712ee76bf8790a
hanumesatru <hajumesatru@gmail.com>
2026-05-30 16:03:52 +0700

    Update genres.js

--- src/data/genres.js
+++ src/data/genres.js
@@ -7,14 +7,12 @@ export const genres = [
     slug: "action-adventure"
   },
   {
-
     id: 16,
     name: "Animation",
     name_th: "แอนิเมชัน",
     slug: "animation"
   },
   {
-
     id: 35,
     name: "Comedy",
     name_th: "คอมเมดี้",
@@ -28,42 +26,36 @@ export const genres = [
     slug: "crime"
   },
   {
-
     id: 99,
     name: "Documentary",
     name_th: "สารคดี",
     slug: "documentary"
   },
   {
-
     id: 18,
     name: "Drama",
     name_th: "ดราม่า",
     slug: "drama"
   },
   {
-
     id: 10751,
     name: "Family",
     name_th: "ครอบครัว",
     slug: "family"
   },
   {
-
     id: 10762,
     name: "Kids",
     name_th: "เด็ก",
     slug: "kids"
   },
   {
-
     id: 9648,
     name: "Mystery",
     name_th: "ลึกลับ",
     slug: "mystery"
   },
   {
-
     id: 10763,
     name: "News",
     name_th: "ข่าว",
@@ -76,35 +68,30 @@ export const genres = [
     slug: "reality"
   },
   {
-
     id: 10765,
     name: "Sci-Fi & Fantasy",
     name_th: "ไซไฟ & แฟนตาซี",
     slug: "sci-fi-fantasy"
   },
   {
-
     id: 10766,
     name: "Soap",
     name_th: "ละครน้ำเน่า",
     slug: "soap"
   },
   {
-
     id: 10767,
     name: "Talk",
     name_th: "ทอล์คโชว์",
     slug: "talk"
   },
   {
-
     id: 10768,
     name: "War & Politics",
     name_th: "สงคราม & การเมือง",
     slug: "war-politics"
   },
   {
-
     id: 37,
     name: "Western",
     name_th: "คาวบอยตะวันตก",
@@ -142,7 +129,6 @@ export const genres = [
     slug: "bl"
   },
   {
-
     id: 100002,
     name: "Girl Love (GL)",
     name_th: "เกิร์ลเลิฟ (GL)",
