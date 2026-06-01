c9c07d97bf68b771bb2774822dcf80c5704d973e
hajumesatru-eng <hajumesatru@gmail.com>
2026-06-01 13:49:41 +0700

    fix: add supabase env for cloudflare

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sjztqmpupmpkpfgverny.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqenRxbXB1cG1wa3BmZ3Zlcm55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMTc5MzcsImV4cCI6MjA5NTY5MzkzN30.gdlKdKXnbZu93RSbOivkTimy-ULHmbv6pofq-HmT_wA";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_ANON_KEY
+);
