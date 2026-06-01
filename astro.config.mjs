632b9d8e92dbf065663b4f6cd4b121f487feb237
hajumesatru-eng <hajumesatru@gmail.com>
2026-06-01 22:52:24 +0700

    static test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  site: "https://wow-drama24.pages.dev",
-
-  output: "server",
-
-  adapter: cloudflare(),
-
-  integrations: [
-    sitemap()
-  ]
+  site: "https://wow-drama24-v2.pages.dev",
+  output: "static",
+  integrations: [sitemap()]
 });
