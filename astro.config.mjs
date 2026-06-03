7f391444dc8ab7263723a30e2907e0ced7d99c67
hajumesatru-eng <hajumesatru@gmail.com>
2026-06-03 23:35:22 +0700

    Update adapter to Vercel and ignore .vercel folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'; // Gunakan ini untuk SSR
+import vercel from '@astrojs/vercel'; // Coba hapus "/serverless" di sini
 
 export default defineConfig({
-  output: 'server', // Wajib untuk SSR/Supabase
+  output: 'server',
   adapter: vercel(),
- 
-  integrations: [sitemap()],
-  site: 'https://wowdrama24.is-cool.dev', // Pastikan menggunakan domain Anda
 });
