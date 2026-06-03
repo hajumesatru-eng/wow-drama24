74e24c420ddd82bbfbb75ec06907edcc8582dbee
hajumesatru-eng <hajumesatru@gmail.com>
2026-06-03 22:39:06 +0700

    Switch adapter to @astrojs/vercel/serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,10 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
-import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless'; // Gunakan ini untuk SSR
 
 export default defineConfig({
-  output: 'server', 
-  
-  adapter: cloudflare({
-    imageService: 'passthrough',
-    
-    // PERBAIKAN UTAMA: Cegah Astro membuat binding otomatis yang memicu error Cloudflare
-    platformProxy: {
-      enabled: false,
-    },
-    
-    // Matikan pencarian otomatis KV Session bawaan agar tidak memicu error kv_namespaces[0]
-    runtime: {
-      mode: 'complete',
-      binding: false,
-    }
-  }),
-  
+  output: 'server', // Wajib untuk SSR/Supabase
+  adapter: vercel(),
+ 
   integrations: [sitemap()],
   site: 'https://wowdrama24.is-cool.dev', // Pastikan menggunakan domain Anda
 });
